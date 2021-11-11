59e1042ad3ed896d6eb80c6d2ffbb6ef86acabd6
HeDeHeng <cooldeheng@gmail.com>
2021-11-11 21:13:00 +0400

    增加更新f2pool.com上的单位算力的产出

--- MinerModel.py
+++ MinerModel.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 import pygsheets
-
+import requests
+import json
 import okex.Market_api as Market
 import time
 import Config
@@ -34,6 +35,32 @@ dcr_last_price = float(result['data'][0]["last"])
 result = marketAPI.get_ticker('KSM-USDT')
 ksm_last_price = float(result['data'][0]["last"])
 
+
+#获取算力
+def get_per(coin):
+    url = 'https://www.f2pool.com/coins-chart'
+    d = {'currency_code': coin, 'history_days': '30d', 'interval': '60m'}
+    r = requests.post(url, data=d)
+    r_dic = json.loads(r.text)
+    if r_dic['data']['profit_per_hash'] == 'K':
+        return float(r_dic['data']['profit_per_t']) / 1000000000
+    elif r_dic['data']['profit_per_hash'] == 'M':
+        return float(r_dic['data']['profit_per_t']) / 1000000
+    elif r_dic['data']['profit_per_hash'] == 'G':
+        return float(r_dic['data']['profit_per_t']) / 1000
+    else:
+        return r_dic['data']['profit_per_t']
+
+
+btc_per = get_per("btc")
+eth_per = get_per("eth")
+zec_per = get_per("zec")
+dash_per = get_per("dash")
+dcr_per = get_per("dcr")
+
+
+
+
 #打开谷歌表格API的权限文件
 client = pygsheets.authorize(service_file = "maneger.json")
 # 打开谷歌表格
@@ -49,5 +76,15 @@ wks.update_value('B6', zec_last_price)
 wks.update_value('B7', dash_last_price)
 wks.update_value('B8', dcr_last_price)
 wks.update_value('B9', ksm_last_price)
+
+wks.update_value('D4', btc_per)
+wks.update_value('D5', eth_per)
+wks.update_value('D6', zec_per)
+wks.update_value('D7', dash_per)
+wks.update_value('D8', dcr_per)
+
+
+
+
 
 
